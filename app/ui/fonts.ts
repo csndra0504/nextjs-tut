2def58e4e03ea339eaa0ce31718c732abf41e9ba
Cassandra Wilcox <cassie@codehangar.io>
2024-03-15 10:36:45 -0400

    lessons 1-5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Aleo, Josefin_Sans } from 'next/font/google';
+
+export const aleo = Aleo({ subsets: ['latin'] });
+export const josafin_sans = Josefin_Sans({
+    weight: ['400', '700'],
+    subsets: ['latin']
+});
